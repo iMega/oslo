01b085da1f3491e7fb0c8ae767c5a46f1dc24152
Dmitry <irvis@imega.ru>
2016-07-09 19:58:03 +0300

    fix daemon

--- src/daemon.sh
+++ src/daemon.sh
@@ -5,7 +5,7 @@
 inotifywait -mr -e close_write --fromfile /app/wait-list.txt | while read DEST EVENT FILE
 do
     UUID=`echo $(basename "$DEST")`
-    /app/oslo parse $UUID
+    /app/oslo parse "$UUID" && \
     rsync --inplace -av /tmp/$UUID/dump.sql rsync://fileman:873/storage/$UUID/ && \
     rm -rf ${DEST%?} /tmp/$UUID
 done
